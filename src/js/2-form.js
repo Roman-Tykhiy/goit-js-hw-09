4ab5a6436e7ab1b9b3be5dfaae8b1228121bd24a
Roman_Tykhiy <2rstykhiy@gmail.com>
2024-11-18 13:03:20 +0200

    hw9-2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -6,7 +6,9 @@ let formData = {
 
 const form = document.querySelector(".feedback-form");
 const input = document.querySelector(".form-input");
-const textarea = document.querySelector(".form-textarea")
+const textarea = document.querySelector(".form-textarea");
+input.value.trim();
+textarea.value.trim();
 
 
 form.addEventListener("input", formInput);
@@ -14,9 +16,9 @@ form.addEventListener("input", formInput);
 function formInput(event) {
    
     if (event.target.classList.contains("form-input")) {
-         formData.email = event.target.value;
+         formData.email = event.target.value.trim();
     } else if (event.target.classList.contains("form-textarea")) {
-        formData.message = event.target.value;
+        formData.message = event.target.value.trim();
     }
     localStorage.setItem("feedback-form-state", JSON.stringify(formData));
 }
@@ -26,10 +28,10 @@ function beakText() {
    
     
     if (textStorage) {
-        input.value = textStorage.email;
-        textarea.value = textStorage.message;
-        formData.email = textStorage.email;
-        formData.message = textStorage.message;
+        input.value = textStorage.email.trim();
+        textarea.value = textStorage.message.trim();
+        formData.email = textStorage.email.trim();
+        formData.message = textStorage.message.trim();
     }   
 }
 beakText();
@@ -37,7 +39,7 @@ beakText();
 form.addEventListener("submit", handSabmit);
 function handSabmit(event) {
     event.preventDefault();
-    if (input.value.length === 0 || textarea.value.length === 0) {
+    if (input.value.trim().length === 0 || textarea.value.trim().length === 0) {
          alert("Fill please all fields")
         return
     }
